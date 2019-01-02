1ecb6bc446c4876446ac90cd509cb283875c7516
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2019-01-01 17:23:10 -0800

    Add basic logging and middleware

--- twitchers-client/app.js
+++ twitchers-client/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const webrtc = require('./routes/webrtc');
+
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
+// Middleware
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`)
+// ROUTES
+app.use('/webrtc/', webrtc);
+
+app.listen(3000, () => {
+    console.log('Random Twitchers Play Client is listening...');
 });
